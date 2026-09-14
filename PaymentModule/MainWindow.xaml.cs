9d0ed52423e0b58f104bbaa1c09c13a50e7c526c
vena <popalvvenu2@gmail.com>
2026-09-14 12:28:23 +0300

    update contract panel

--- PaymentModule/MainWindow.xaml.cs
+++ PaymentModule/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -27,6 +28,7 @@ namespace PaymentModule
         {
             InitializeComponent();
         }
+        public static bool contractIsOpen = false;
         private void NavList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             //скрыть панели
@@ -40,7 +42,14 @@ namespace PaymentModule
             contract3.Visibility = Visibility.Collapsed;
             contract4.Visibility = Visibility.Collapsed;
 
+            contractV1.Visibility = Visibility.Collapsed;
+            contractV2.Visibility = Visibility.Collapsed;
+            contractV3.Visibility = Visibility.Collapsed;
+            contractV4.Visibility = Visibility.Collapsed;
 
+            panelContract.Width = new GridLength(0, GridUnitType.Pixel);
+            contractIsOpen = false;
+            
             //показываем
             switch (NavList.SelectedIndex)
             {
@@ -53,66 +62,80 @@ namespace PaymentModule
 
         private void contract1_Click(object sender, RoutedEventArgs e)
         {
-            //показываем контракт       
-            contract1.Visibility = Visibility.Visible;
-            //скрываем текущую панель  
-            Panel1.Visibility = Visibility.Collapsed;
-        }
-
-        private void hideContract1_Click(object sender, RoutedEventArgs e)
-        {
-            //скрываем контракт        
-            contract1.Visibility = Visibility.Collapsed;
-            //показываем текущую панель
-            Panel1.Visibility = Visibility.Visible;
+            if (contractIsOpen)
+            {
+                //скрываем контракт        
+                contract1.Visibility = Visibility.Collapsed;
+                contractV1.Visibility = Visibility.Collapsed;
+                panelContract.Width = new GridLength(0, GridUnitType.Pixel);
+            }else
+            {
+                //показываем контракт       
+                contract1.Visibility = Visibility.Visible;
+                contractV1.Visibility = Visibility.Visible;
+                panelContract.Width = new GridLength(1.5, GridUnitType.Star);
+            }
+            contractIsOpen = contractIsOpen == false;
         }
 
         private void contract2_Click(object sender, RoutedEventArgs e)
         {
-            //показываем контракт      
-            contract2.Visibility = Visibility.Visible;
-            //скрываем текущую панель  
-            Panel2.Visibility = Visibility.Collapsed;
+            if (contractIsOpen)
+            {
+                //скрываем контракт        
+                contract2.Visibility = Visibility.Collapsed;
+                contractV2.Visibility = Visibility.Collapsed;
+                panelContract.Width = new GridLength(0, GridUnitType.Pixel);
+            }
+            else
+            {
+                //показываем контракт       
+                contract2.Visibility = Visibility.Visible;
+                contractV2.Visibility= Visibility.Visible;
+                panelContract.Width = new GridLength(1.5, GridUnitType.Star);
+            }
+            contractIsOpen = contractIsOpen == false;
         }
 
-        private void hideContract2_Click(object sender, RoutedEventArgs e)
-        {
-            //скрываем контракт        
-            contract2.Visibility = Visibility.Collapsed;
-            //показываем текущую панель
-            Panel2.Visibility = Visibility.Visible;
-        }
 
         private void contract3_Click(object sender, RoutedEventArgs e)
         {
-            //показываем контракт      
-            contract3.Visibility = Visibility.Visible;
-            //скрываем текущую панель  
-            Panel3.Visibility = Visibility.Collapsed;
+            if (contractIsOpen)
+            {
+                //скрываем контракт        
+                contract3.Visibility = Visibility.Collapsed;
+                contractV3.Visibility = Visibility.Collapsed;
+                panelContract.Width = new GridLength(0, GridUnitType.Pixel);
+            }
+            else
+            {
+                //показываем контракт       
+                contract3.Visibility = Visibility.Visible;
+                contractV3.Visibility = Visibility.Visible;
+                panelContract.Width = new GridLength(1.5, GridUnitType.Star);
+            }
+            contractIsOpen = contractIsOpen == false;
         }
 
-        private void hideContract3_Click(object sender, RoutedEventArgs e)
-        {
-            //скрываем контракт        
-            contract3.Visibility = Visibility.Collapsed;
-            //показываем текущую панель
-            Panel3.Visibility = Visibility.Visible;
-        }
 
         private void contract4_Click(object sender, RoutedEventArgs e)
         {
-            //показываем контракт      
-            contract4.Visibility = Visibility.Visible;
-            //скрываем текущую панель  
-            Panel4.Visibility = Visibility.Collapsed;
+            if (contractIsOpen)
+            {
+                //скрываем контракт        
+                contract4.Visibility = Visibility.Collapsed;
+                contractV4.Visibility = Visibility.Collapsed;
+                panelContract.Width = new GridLength(0, GridUnitType.Pixel);
+            }
+            else
+            {
+                //показываем контракт       
+                contract4.Visibility = Visibility.Visible;
+                contractV4.Visibility = Visibility.Visible;
+                panelContract.Width = new GridLength(1.5, GridUnitType.Star);
+            }
+            contractIsOpen = contractIsOpen == false;
         }
 
-        private void hideContract4_Click(object sender, RoutedEventArgs e)
-        {
-            //скрываем контракт        
-            contract4.Visibility = Visibility.Collapsed;
-            //показываем текущую панель
-            Panel4.Visibility = Visibility.Visible;
-        }
     }
 }
